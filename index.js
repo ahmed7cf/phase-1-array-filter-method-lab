01297579dea8d420254804a61af139b8d32479a8
Ahmed Ibrahim Ahmed <ahmed7cf@gmail.com>
2023-10-20 20:16:10 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function findMatching(array, driverName) {
+    let collection = []
+    array.filter(function (name) {
+        if (name.toLowerCase() === driverName.toLowerCase()) {
+            collection.push(name)
+        }
+    })
+    return collection
+}
+
+function fuzzyMatch(array, driverName) {
+    let collection = []
+    array.filter(function (name) {
+        if (name.startsWith(driverName)) {
+            collection.push(name)
+        }
+    })
+    return collection
+}
+
+function matchName(array, driverName) {
+    let collection = []
+    array.filter(function (name) {
+        if (name.name === driverName) {
+            collection.push(name)
+        }
+    })
+    return collection
+}
